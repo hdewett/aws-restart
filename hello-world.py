7588aae7af5ac20588eb1cd7e068710e06b0cb14
Hilora <105822645+hdewett@users.noreply.github.com>
2022-08-02 14:45:24 -0400

    Add files via upload
    Beginner python files from aws re/start course.

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+print("Hello, World")
+print("My dog, Ballsmoi, is a good boy.")
+dog=""
+while dog != "Ballsmoi":
+    dog = input("What is you favourite dog's name?")
+    if dog == "Ballsmoi":
+         print("He is a very good boy!")
+    else:
+        print("Who is that?")
+    
